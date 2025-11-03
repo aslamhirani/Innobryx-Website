277c49c57e4e4b5a4392b54c302d3c88da98fd60
aslamhirani <aslam.hirani@gmail.com>
2025-11-03 09:07:12 +0530

    Add base path to Vite configuration
    Set base path for Vite configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+export default {
+  base: './',
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
